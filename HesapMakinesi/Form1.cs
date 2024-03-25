fe9c6a1760e207418fa30c9a37cf76f9b6d476d0
ysmny <ysmny@DESKTOP-4IR07KV>
2024-03-25 13:21:15 +0300

    Proje dosyası ekle.

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Toplama İşlemi için 
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 + sayi2;
+            textBox3.Text = sonuc.ToString();
+            
+        }
+        //Çıkartma
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 - sayi2;
+            textBox3.Text = sonuc.ToString();
+
+        }
+        //Çarpma
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            double sonuc = sayi1 * sayi2;
+            textBox3.Text = sonuc.ToString();
+
+        }
+        //Bölme
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi1 = Convert.ToDouble(textBox1.Text);
+            double sayi2 = Convert.ToDouble(textBox2.Text);
+            if(sayi2 !=0)
+            {
+                double sonuc = sayi1 / sayi2;
+                textBox3.Text = sonuc.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Sıfıra bölüm tanımsızdır.");
+            }
+           
+        }
+    }
+}
